e5c404ad605540511c42cdac30115bff0e702968
CN06QQ2 <CN06QQ2@192.168.101.53>
2022-06-17 09:59:47 +0900

    add: 従業員editアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -18,22 +18,22 @@ import services.EmployeeService;
  *
  */
 public class EmployeeAction extends ActionBase {
-	
+
 	private EmployeeService service;
-	
+
 	/**
 	 * メソッドを実行する
 	 */
 	@Override
 	public void process() throws ServletException, IOException {
 		service = new EmployeeService();
-		
+
 		// メソッドを実行
 		invoke();
-		
+
 		service.close();
 	}
-	
+
 	/**
 	 * 一覧画面を表示する
 	 * @throws ServletException
@@ -43,26 +43,26 @@ public class EmployeeAction extends ActionBase {
 		// 指定されたページ数の一覧画面に表示するデータを取得
 		int page = getPage();
 		List<EmployeeView> employees = service.getPerPage(page);
-		
+
 		// すべての従業員データの件数を取得
 		long employeeCount = service.countAll();
-		
+
 		putRequestScope(AttributeConst.EMPLOYEES, employees); //取得した従業員データ
 		putRequestScope(AttributeConst.EMP_COUNT, employeeCount); //すべての従業員データの件数
 		putRequestScope(AttributeConst.PAGE, page); // ページ数
 		putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE); //1ページに表示するレコードの数
-		
+
 		// セッションにフラッシュメッセージが設定されている場合は、リクエストスコープに移し替え、セッションからは削除する
 		String flush = getSessionScope(AttributeConst.FLUSH);
 		if (flush != null) {
 			putRequestScope(AttributeConst.FLUSH, flush);
 			removeSessionScope(AttributeConst.FLUSH);
 		}
-		
+
 		// 一覧画面を表示
 		forward(ForwardConst.FW_EMP_INDEX);
 	}
-	
+
 	/**
 	 * 新規登録画面を表示する
 	 * @throws ServletException
@@ -71,7 +71,7 @@ public class EmployeeAction extends ActionBase {
 	public void entryNew() throws ServletException, IOException {
 		putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSR対策用トークン
 		putRequestScope(AttributeConst.EMPLOYEE, new EmployeeView()); // 空の従業員インスタンス
-		
+
 		// 新規登録画面を表示
 		forward(ForwardConst.FW_EMP_NEW);
 	}
@@ -82,10 +82,10 @@ public class EmployeeAction extends ActionBase {
 	 * @throws IOException
 	 */
 	public void create() throws ServletException, IOException {
-		
+
 		// CSRF対策 tokenのチェック
 		if (checkToken()) {
-			
+
 			// パラメタの値をもとに従業員情報のインスタンスを作成する
 			EmployeeView ev = new EmployeeView(
 					null,
@@ -96,58 +96,79 @@ public class EmployeeAction extends ActionBase {
 					null,
 					null,
 					AttributeConst.DEL_FLAG_FALSE.getIntegerValue());
-			
+
 			// アプリケーションスコープからpeper文字列を取得
 			String pepper = getContextScope(PropertyConst.PEPPER);
-			
+
 			// 従業員情報登録
-			List<String> errors = service.create(ev,  pepper);
-			
+			List<String> errors = service.create(ev, pepper);
+
 			if (errors.size() > 0) {
 				// 登録中にエラーがあった場合
-				
+
 				putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
 				putRequestScope(AttributeConst.EMPLOYEE, ev); // 入力された従業員情報
 				putRequestScope(AttributeConst.ERR, errors); // エラーのリスト
-				
+
 				// 新規登録画面を再表示
 				forward(ForwardConst.FW_EMP_NEW);
-				
+
 			} else {
 				// 登録中にエラーがなかった場合
-				
+
 				// セッションに登録完了のフラッシュメッセージを設定
 				putSessionScope(AttributeConst.FLUSH, MessageConst.I_REGISTERED.getMessage());
-				
+
 				//  一覧画面にリダイレクト
 				redirect(ForwardConst.ACT_EMP, ForwardConst.CMD_INDEX);
 			}
-					
+
 		}
 	}
 
-	
 	/**
 	 * 詳細画面を表示する
 	 * @throws ServletException
 	 * @throws IOException
 	 */
 	public void show() throws ServletException, IOException {
-		
+
 		// idを条件に従業員データを取得する
 		EmployeeView ev = service.findOne(toNumber(getRequestParam(AttributeConst.EMP_ID)));
-		
+
 		if (ev == null || ev.getDeleteFlag() == AttributeConst.DEL_FLAG_TRUE.getIntegerValue()) {
-			
+
 			// データが取得できなかった、または論理削除されている場合はエラー画面を表示
 			forward(ForwardConst.FW_ERR_UNKNOWN);
 			return;
 		}
-		
+
 		putRequestScope(AttributeConst.EMPLOYEE, ev); // 取得した従業員情報
-		
+
 		// 詳細画面を表示
 		forward(ForwardConst.FW_EMP_SHOW);
 	}
 
+	/**
+	 * 編集画面を表示する
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	public void edit() throws ServletException, IOException {
+		// idを条件に従業員データを取得する
+		EmployeeView ev = service.findOne(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+
+		if (ev == null || ev.getDeleteFlag() == AttributeConst.DEL_FLAG_TRUE.getIntegerValue()) {
+
+			// データが取得できなかった、または論理削除されている場合はエラー画面を表示
+			forward(ForwardConst.FW_ERR_UNKNOWN);
+			return;
+		}
+
+		putRequestScope(AttributeConst.TOKEN, getTokenId());
+		putRequestScope(AttributeConst.EMPLOYEE, ev); // 取得した従業員情報
+
+		// 編集画面を表示する
+		forward(ForwardConst.FW_EMP_EDIT);
+	}
 }
